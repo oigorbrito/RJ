7e7e2e1ddbde4037a0a72b61c5189b320cc83668
tihotm <igorbrito.n1@hotmail.com>
2026-09-04 11:53:27 -0300

    refactor(persistence): remove infrastructure conflict exception

--- src/RJ.Infrastructure/Persistence/LegalDocumentPersistenceConflictException.cs
+++
@@ -1,9 +0,0 @@
-namespace RJ.Infrastructure.Persistence;
-
-public sealed class LegalDocumentPersistenceConflictException : InvalidOperationException
-{
-    public LegalDocumentPersistenceConflictException(string message)
-        : base(message)
-    {
-    }
-}
