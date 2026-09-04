f5b7214184aa5aaf7c83acdbd628b4fda91772c5
tihotm <igorbrito.n1@hotmail.com>
2026-09-04 10:18:05 -0300

    test: make architecture assembly references explicit

--- tests/RJ.ArchitectureTests/DependencyRulesTests.cs
+++ tests/RJ.ArchitectureTests/DependencyRulesTests.cs
@@ -8,7 +8,7 @@ public sealed class DependencyRulesTests
     public void Domain_must_not_reference_other_RJ_projects()
     {
         AssertNoUnexpectedProjectReferences(
-            typeof(Domain.AssemblyMarker).Assembly,
+            typeof(global::RJ.Domain.AssemblyMarker).Assembly,
             allowedProjectReferences: []);
     }
 
@@ -16,7 +16,7 @@ public sealed class DependencyRulesTests
     public void Application_may_reference_only_Domain()
     {
         AssertNoUnexpectedProjectReferences(
-            typeof(Application.AssemblyMarker).Assembly,
+            typeof(global::RJ.Application.AssemblyMarker).Assembly,
             allowedProjectReferences: ["RJ.Domain"]);
     }
 
@@ -24,7 +24,7 @@ public sealed class DependencyRulesTests
     public void Infrastructure_may_reference_only_Application_and_Domain()
     {
         AssertNoUnexpectedProjectReferences(
-            typeof(Infrastructure.AssemblyMarker).Assembly,
+            typeof(global::RJ.Infrastructure.AssemblyMarker).Assembly,
             allowedProjectReferences: ["RJ.Application", "RJ.Domain"]);
     }
 
@@ -32,7 +32,7 @@ public sealed class DependencyRulesTests
     public void Api_may_reference_Application_and_Infrastructure_but_not_Domain_directly()
     {
         AssertNoUnexpectedProjectReferences(
-            typeof(Api.AssemblyMarker).Assembly,
+            typeof(global::RJ.Api.AssemblyMarker).Assembly,
             allowedProjectReferences: ["RJ.Application", "RJ.Infrastructure"]);
     }
 
