7e695206987b612996593783dd3dabbd81141032
tihotm <igorbrito.n1@hotmail.com>
2026-09-04 11:48:54 -0300

    feat(benchmark): route catalog validation command

--- src/RJ.BenchmarkCli/Program.cs
+++ src/RJ.BenchmarkCli/Program.cs
@@ -1,3 +1,10 @@
 using RJ.BenchmarkCli;
 
+if (args.Length >= 2
+    && StringComparer.Ordinal.Equals(args[0], "catalog")
+    && StringComparer.Ordinal.Equals(args[1], "validate"))
+{
+    return await CorpusAdmissionCli.RunAsync(args[2..], CancellationToken.None);
+}
+
 return await BenchmarkCli.RunAsync(args, CancellationToken.None);
