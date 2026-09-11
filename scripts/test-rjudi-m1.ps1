0aa2874cbba9750169d1dd91fd8c735d8aafdec3
oigorbrito <oigorbrito@gmail.com>
2026-09-11 01:15:31 -0300

    Include operational security boundary in canonical M1 gate

--- scripts/test-rjudi-m1.ps1
+++ scripts/test-rjudi-m1.ps1
@@ -9,7 +9,7 @@ $apiProjectPath = Join-Path $repoRoot 'tests\RJ.ApiTests\RJ.ApiTests.csproj'
 $apiBuildProjectPath = Join-Path $repoRoot 'src\RJ.Api\RJ.Api.csproj'
 
 $domainFilter = 'FullyQualifiedName~RJ.DomainTests.ProcessSummaryCorrectiveRetryTests|FullyQualifiedName~RJ.DomainTests.ProcessSummaryRefreshPlannerTests|FullyQualifiedName~RJ.DomainTests.ProcessSummaryObservabilityCatalogTests|FullyQualifiedName~RJ.DomainTests.ProcessAttachmentContentAdmissionTests|FullyQualifiedName~RJ.DomainTests.RjudiProcessBenchmarkCatalogFactoryTests|FullyQualifiedName~RJ.DomainTests.LegalCaseConsistencyEngineTests|FullyQualifiedName~RJ.DomainTests.ProcessGenerationContextComposerTests|FullyQualifiedName~RJ.DomainTests.ProcessSummaryJobServiceTests|FullyQualifiedName~RJ.DomainTests.ProcessSecurityPolicyTests|FullyQualifiedName~RJ.DomainTests.ProcessSummaryValidatorTests|FullyQualifiedName~RJ.DomainTests.ProcessSummaryGenerationTests|FullyQualifiedName~RJ.DomainTests.LegalCaseMergeServiceTests|FullyQualifiedName~RJ.DomainTests.ProcessNormalizationTests|FullyQualifiedName~RJ.DomainTests.JuditProcessSourceAdapterTests|FullyQualifiedName~RJ.DomainTests.ProcessSourceCanonicalizationServiceTests|FullyQualifiedName~RJ.DomainTests.Wave1CorpusContractTests|FullyQualifiedName~RJ.DomainTests.LegalCaseTests|FullyQualifiedName~RJ.DomainTests.LegalDocumentTests'
-$apiFilter = 'FullyQualifiedName~RJ.ApiTests.ProcessSummaryEndpointTests'
+$apiFilter = 'FullyQualifiedName~RJ.ApiTests.ProcessSummaryEndpointTests|FullyQualifiedName~RJ.ApiTests.ProcessSummarySecurityBoundaryTests'
 
 function Invoke-GateStep {
     param(
@@ -30,7 +30,7 @@ Push-Location $repoRoot
 try {
     Invoke-GateStep 'build RJ.Api' { dotnet build $apiBuildProjectPath --no-restore }
     Invoke-GateStep 'domain RJudi M1 tests' { dotnet test $domainProjectPath --no-restore --filter $domainFilter }
-    Invoke-GateStep 'api RJudi endpoint tests' { dotnet test $apiProjectPath --no-restore --filter $apiFilter }
+    Invoke-GateStep 'api RJudi endpoint/security tests' { dotnet test $apiProjectPath --no-restore --filter $apiFilter }
     Invoke-GateStep 'git diff whitespace check' { git diff --check }
 }
 finally {
